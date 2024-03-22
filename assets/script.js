04e194d4bb904292b4ccf395ca6d88653e235472
thedorktrain <mackemosabe@gmail.com>
2024-03-22 15:19:32 -0500

    added JS to project

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomtopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(let x = 0; x <topics.length; x++)
-console.log(topics[x])
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
 
-/*
-if (topics === 'HTML') {
+function selectTopic() {
+if (randomtopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (randomtopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (randomtopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (randomtopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}
-*/
+}}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
